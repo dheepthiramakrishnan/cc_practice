23b8e5dd481e973d312fcf7eace44b8cbbd5520c
25mx118 <25mx118@MCA.PSG>
2026-07-07 11:07:06 +0530

    Feature: Added areaTriangle calculation

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -1,5 +1,3 @@
-/ Team Collaboration Project: Area Calculator
-// Author: Partner 1 (Base Setup & Circle Feature)
 import java.util.Scanner;
 
 public class AreaCalculator {
@@ -9,25 +7,44 @@ public class AreaCalculator {
         return Math.PI * Math.pow(radius, 2);
     }
 
+    // Partner 3 Feature
+    public static double areaTriangle(double base, double height) {
+        return 0.5 * base * height;
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.util.Scanner.in);
-        
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("--- Collaborative Area Calculator ---");
         System.out.println("1. Circle");
         System.out.println("2. Rectangle (Coming Soon)");
-        System.out.println("3. Triangle (Coming Soon)");
-        
+        System.out.println("3. Triangle");
+
         System.out.print("\nChoose a shape (1-3): ");
         String choice = scanner.next();
-        
+
         if (choice.equals("1")) {
+
             System.out.print("Enter radius: ");
             double r = scanner.nextDouble();
             System.out.printf("👉 Area of Circle: %.2f\n", areaCircle(r));
+
+        } else if (choice.equals("3")) {
+
+            System.out.print("Enter base: ");
+            double b = scanner.nextDouble();
+
+            System.out.print("Enter height: ");
+            double h = scanner.nextDouble();
+
+            System.out.printf("👉 Area of Triangle: %.2f\n", areaTriangle(b, h));
+
         } else {
+
             System.out.println("Feature not implemented yet or invalid choice.");
         }
-        
+
         scanner.close();
     }
 }
